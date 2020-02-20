b7d88caaaba5c1da302985f7f1ef140f3925f819
Tom <github@tomversteeg.nl>
2020-02-20 14:53:17 +0100

    init

---
+++ resources/views/homepage/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <section id="header">
+        <div class="container">
+            <div id="logo">
+                <a href="{{ asset('/') }}">Klusjes<span class="contrast">Zwolle</span>.nl</a>
+            </div>
+        </div>
+        <nav>
+
+        </nav>
+    </section>
+    <section id="call-to-action">
+
+    </section>
+</body>
+</html>
