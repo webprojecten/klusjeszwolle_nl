8bd31ad39d76b9779c2caf75aa6c60f31e071219
Tom <github@tomversteeg.nl>
2020-02-21 16:01:17 +0100

    eindelijk mooi ontwerp

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -25,7 +25,7 @@
         </div>
     </div>
     <div id="header-buffer"></div>
-    <div class="container">
+    <div class="container" id="content-wrapper">
         <div id="call-to-action">
             <div id="cta-voordelen">
                 <h2>Voordelen van klusjeszwolle.nl</h2>
@@ -48,7 +48,62 @@
                 </div>
             </div>
         </div>
+        <h1 class="primair">KlusjesZwolle.nl: wij staan dag en nacht klaar voor al uw klussen in Zwolle</h1>
+        <div class="icons">
+            
+        </div>
+        <div class="content">
+            <img src="{{ asset('/img/klusjes-in-zwolle.jpg') }}" alt="Klusjes in Zwolle" title="Klusjes in Zwolle">
+            <article>
+                <h2 class="primair">Klussenbedrijf in Zwolle zoeken?</h2>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos neque voluptatem sequi totam nam laudantium at reprehenderit labore repellat, numquam debitis ipsam necessitatibus repudiandae, voluptas nisi consectetur explicabo corrupti quia facere natus fuga velit omnis odit mollitia. Magnam sunt, eveniet porro aperiam, molestias aliquid cupiditate possimus architecto unde esse corrupti.
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos neque voluptatem sequi totam nam laudantium at reprehenderit labore repellat, numquam debitis ipsam necessitatibus repudiandae, voluptas nisi consectetur explicabo corrupti quia facere natus fuga velit omnis odit mollitia. Magnam sunt, eveniet porro aperiam, molestias aliquid cupiditate possimus architecto unde esse corrupti.
+                </p>
+            </article>
+        </div>
+        <div class="content">
+            <article>
+                <h2 class="primair">Klussenbedrijf in Zwolle zoeken?</h2>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos neque voluptatem sequi totam nam laudantium at reprehenderit labore repellat, numquam debitis ipsam necessitatibus repudiandae, voluptas nisi consectetur explicabo corrupti quia facere natus fuga velit omnis odit mollitia. Magnam sunt, eveniet porro aperiam, molestias aliquid cupiditate possimus architecto unde esse corrupti.
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos neque voluptatem sequi totam nam laudantium at reprehenderit labore repellat, numquam debitis ipsam necessitatibus repudiandae, voluptas nisi consectetur explicabo corrupti quia facere natus fuga velit omnis odit mollitia. Magnam sunt, eveniet porro aperiam, molestias aliquid cupiditate possimus architecto unde esse corrupti.
+                </p>
+            </article>
+            <img src="{{ asset('/img/klusjes-in-zwolle.jpg') }}" alt="Klusjes in Zwolle" title="Klusjes in Zwolle">
+
+        </div>
+        <!-- <h2 class="primair">Voorbeelden klussen in Zwolle</h2> -->
+        <div id="mogelijkheden">
+            <img src="{{ asset('/img/badkamer-verbouwen-zwolle.jpg') }}" alt=" in Zwolle" title=" in Zwolle">
+            <img src="{{ asset('/img/keuken-verbouwen-zwolle.jpg') }}" alt=" in Zwolle" title=" in Zwolle">
+            <img src="{{ asset('/img/onderhoud-dak-zwolle.jpg') }}" alt=" in Zwolle" title=" in Zwolle">
+            <img src="{{ asset('/img/onderhoud-electronica-zwolle.jpg') }}" alt=" in Zwolle" title=" in Zwolle">
+            <img src="{{ asset('/img/vloer-leggen-zwolle.jpg') }}" alt=" in Zwolle" title=" in Zwolle">
+            <img src="{{ asset('/img/woonkamer-verbouwen-zwolle.jpg') }}" alt=" in Zwolle" title=" in Zwolle">
+        </div>
+        <div class="content no-border">
+            <img src="{{ asset('/img/klusjes-in-zwolle.jpg') }}" alt="Klusjes in Zwolle" title="Klusjes in Zwolle">
+            <article>
+                <h2 class="primair">Klussenbedrijf in Zwolle zoeken?</h2>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos neque voluptatem sequi totam nam laudantium at reprehenderit labore repellat, numquam debitis ipsam necessitatibus repudiandae, voluptas nisi consectetur explicabo corrupti quia facere natus fuga velit omnis odit mollitia. Magnam sunt, eveniet porro aperiam, molestias aliquid cupiditate possimus architecto unde esse corrupti.
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos neque voluptatem sequi totam nam laudantium at reprehenderit labore repellat, numquam debitis ipsam necessitatibus repudiandae, voluptas nisi consectetur explicabo corrupti quia facere natus fuga velit omnis odit mollitia. Magnam sunt, eveniet porro aperiam, molestias aliquid cupiditate possimus architecto unde esse corrupti.
+                </p>
+            </article>
+        </div>
     </div>
+    <footer>
+        <div class="container">
+            @ 2020
+        </div>
+    </footer>
     <script src="{{ asset('js/js_events.js') }}"></script>
 </body>
 </html>
