4b28cf3d99076789a75a3d86d52a928fc89d9754
Tom <github@tomversteeg.nl>
2020-02-23 00:55:52 +0100

    het werkt geen js

--- resources/views/filmpje/index.blade.php
+++ resources/views/filmpje/index.blade.php
@@ -8,25 +8,35 @@
 <body>
 
     <div id="filmpje-container">
+        <!-- todo met js aansturen dat het 'filmpje' aangaat als alle images geladen zijn o.t.m. de eerste paar -->
         <div id="filmpje" style="display: flex;">
-            <img id="img_1" class="img" src="/img/4.jpg" alt="">
-            <img id="img_2" class="img" src="/img/5.jpg" alt="">
-            <img id="img_3" class="img" src="/img/6.jpg" alt="">
+            <img id="img_1" class="img" src="/img/21.jpg" alt="">
+            <img id="img_2" class="img" src="/img/22.jpg" alt="">
+            <img id="img_3" class="img" src="/img/23.jpg" alt="">
+            <img id="img_4" class="img" src="/img/24.jpg" alt="">
             <!----   Tekst loop voor het filmpje    ---->
             <div id="tekst_1" class="tekst">
-                Hallo allemaal!
+                <p>In mei 1940 werd Nederland binnengevallen door het duitse leger.</p>
             </div>
             <div id="tekst_2" class="tekst">
-                Welkom op de website.
+                <p>Zij wilden totale overheersing van Nederland</p>
             </div>
             <div id="tekst_3" class="tekst">
-                Wat leuk dat dit werkt!
+                <p>Nederland werd hard getroffen door duitse bombardementen</p>
             </div>
             <div id="tekst_4" class="tekst">
-                :)
+                <p>
+                    Het leven in ons land was moeilijk doordat mensen constant werden geraalt door het duitse leger
+                </p>
             </div>
             <div id="tekst_5" class="tekst">
-                Meisje met autosleutels
+                <p>Veel mensen leefden in armoede</p>
+            </div>
+            <div id="tekst_6" class="tekst">
+                <p>Maar er was ook reden tot hoop...</p>
+            </div>
+            <div id="tekst_7" class="tekst">
+                <p>In mei 1945 werd Nederland bevrijd van de Duitsers</p>
             </div>
         </div>
     </div>
@@ -42,33 +52,33 @@
     #filmpje {
         width: 1200px;
         height: 800px;
-        position: relative;
-        overflow:hidden;
+        position: absolute;
+        overflow: hidden;
     }
 
     .tekst {
         position: absolute;
-        top:30%;
-        left: 20%;
-        margin: auto; 
-        color: white;
-        font-size: 7em;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        /* top:30%;
+        left: 20%; */
+        /* margin: auto;  */
+        color: #eee;
+        font-size: 4em;
         font-family: Helvetica, sans-serif;
         font-weight: 900;
         text-shadow: 2px 2px black;
     }
 
-    .img {
-        opacity: 0;
-        width: 1200px;
-        height: 800px;
-        position: absolute;
-
+    .tekst p {
+        width: 70%;
+        margin: auto;
     }
 
+
     #img_1 {
-        opacity: 1 !important;
-        z-index:3;
+        z-index:13;
         -webkit-animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_1;
         animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_1;
         -webkit-animation-fill-mode: forwards;
@@ -76,7 +86,7 @@
     }
 
     #img_2 {
-        z-index:2;
+        z-index:12;
         -webkit-animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_2;
         animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_2;
         -webkit-animation-fill-mode: forwards;
@@ -84,11 +94,26 @@
     }
 
     #img_3 {
-        z-index:1;
+        z-index:11;
         -webkit-animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_3;
         animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_3;
         -webkit-animation-fill-mode: forwards;
         animation-fill-mode: forwards;
+    }
+
+    #img_4 {
+        z-index:10;
+        -webkit-animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_4;
+        animation: <?php echo $duration ?> ease 0s 1 filmpjeAfspelen_4;
+        -webkit-animation-fill-mode: forwards;
+        animation-fill-mode: forwards;
+    }
+    
+    .img {
+        opacity: 0;
+        width: 1200px;
+        height: 800px;
+        position: absolute;
     }
 
     #tekst_1 {
@@ -125,30 +150,49 @@
         animation:  <?php echo $duration ?>  ease 0s 1 filmpjeTekst_5;
         z-index: 100005;
     }
+
+    #tekst_6 {
+        opacity: 0;
+        -webkit-animation:  <?php echo $duration ?>  ease 0s 1 filmpjeTekst_6;
+        animation:  <?php echo $duration ?>  ease 0s 1 filmpjeTekst_6;
+        z-index: 100006;
+    }
+
+    #tekst_7 {
+        opacity: 0;
+        -webkit-animation:  <?php echo $duration ?>  ease 0s 1 filmpjeTekst_7;
+        animation:  <?php echo $duration ?>  ease 0s 1 filmpjeTekst_7;
+        z-index: 100007;
+        -webkit-animation-fill-mode: forwards;
+        animation-fill-mode: forwards;
+    }
     
     @keyframes filmpjeAfspelen_1 {
         0% {
             opacity: 1;
-        }
-        10%{
-        }
-        13% {
-            opacity: 1;
-            width: 180%;
-            height: 180%;
             left: 0;
+            top:0;
+        }
+        15% {
+            opacity: 1;
+            width: 140%;
+            height: 140%;
+            left: -10px;
+            top:-100px;
+        }
+        20% {
+            width: 140%;
+            height: 140%;
+            opacity: 1;
         }
         25% {
-            left: -75%;
-            width: 180%;
-            height: 180%;
-            opacity: 1;
-        }
-        30% {
+            width: 140%;
+            height: 140%;
             opacity:0;
         }
-
         100% {
+            left: -10px;
+            top:-100px;
             opacity:0;
         }
     }
@@ -160,7 +204,7 @@
         5% {
             opacity: 1;
         }
-        25% {
+        20% {
             left:0;
             top:0;
             width: 100%;
@@ -177,6 +221,10 @@
             opacity: 0;
         }
         100% {
+            top: -100px;
+            left: -700px;
+            width: 160%;
+            height: 160%;
             opacity: 0;
         }
     }
@@ -185,8 +233,11 @@
         0% {
             opacity: 0;
         }
-        5% {
-            opacity: 1;
+        30% {
+            opacity: 0;
+        }
+        35% {
+            opacity:1;
         }
         40%{
             top:0;
@@ -195,104 +246,52 @@
             height: 100%;
         }
         75% {
-            width: 105%;
-            height: 105%;
+            width: 115%;
+            height: 115%;
+            top: 0;
+            left: 0;
+            opacity:1;
+        }
+        80%{
+            opacity: 0;
+        }
+        100% {
+            opacity:0;
+            width: 115%;
+            height: 115%;
+            top: 0;
+            left: 0;
+        }
+    }
+
+    @keyframes filmpjeAfspelen_4 {
+        0% {
+            opacity: 0;
+        }
+        30% {
+            opacity: 0;
+        }
+        35% {
+            opacity:1;
+        }
+        40%{
+            top:0;
+            left:0;
+            width: 100%;
+            height: 100%;
+        }
+        75% {
+            width: 100%;
+            height: 100%;
             top: 0;
             left: 0;
         }
         100% {
-            width: 105%;
-            height: 105%;
-            top: 0;
-            left: 0;
-        }
-    }
-
-    @-webkit-keyframes filmpjeAfspelen_1 {
-        0% {
-            /* background-position: 8% 2%; */
-            
-        }
-        23% {
-        }
-        25% {
-            /* background-image: url('/img/4.jpg');
-            background-size: 200%;
-            background-position: 92% 2%; */
-        }
-        30% {
-            /* background-image: url('/img/5.jpg');
-            background-size: 150%; */
-        }
-        40% {
-            /* background-image: url('/img/6.jpg');
-            background-size: 100%;
-            background-position: 50% 50%; */
-        }
-        100% {
-            /* background-image: url('/img/6.jpg');
-            background-size: 100%;
-            background-position: 0% 0%; */
-        }
-    }
-
-    @-webkit-keyframes filmpjeAfspelen_2 {
-        0% {
-            /* opacity: 1; */
-            /* background-position: 8% 2%; */
-            
-        }
-        5% {
-            /* opacity: 1; */
-        }
-        25% {
-            /* background-image: url('/img/4.jpg');
-            background-size: 200%;
-            background-position: 92% 2%; */
-        }
-        30% {
-            /* background-image: url('/img/5.jpg');
-            background-size: 150%; */
-        }
-        40% {
-            /* background-image: url('/img/6.jpg');
-            background-size: 100%;
-            background-position: 50% 50%; */
-        }
-        100% {
-            /* background-image: url('/img/6.jpg');
-            background-size: 100%;
-            background-position: 0% 0%; */
-        }
-    }
-
-    @-webkit-keyframes filmpjeAfspelen_3 {
-        0% {
-            /* opacity: 1;
-            background-position: 8% 2%; */
-            
-        }
-        5% {
-            /* opacity: 1; */
-        }
-        25% {
-            /* background-image: url('/img/4.jpg');
-            background-size: 200%;
-            background-position: 92% 2%; */
-        }
-        30% {
-            /* background-image: url('/img/5.jpg');
-            background-size: 150%; */
-        }
-        40% {
-            /* background-image: url('/img/6.jpg');
-            background-size: 100%;
-            background-position: 50% 50%; */
-        }
-        100% {
-            /* background-image: url('/img/6.jpg');
-            background-size: 100%;
-            background-position: 0% 0%; */
+            opacity:1;
+            width: 145%;
+            height: 145%;
+            top: -200px;
+            left: -200px;
         }
     }
 
@@ -307,11 +306,10 @@
         7%{
             opacity: 1;
         }
-        7.1%{
+        10%{
             opacity: 1;
         }
-        9.1%{
-            display: none;
+        12%{
             opacity: 0
         }
         100% {
@@ -324,16 +322,16 @@
         0% {
             opacity: 0;
         }
-        10%{
+        13%{
             opacity: 0;
         }
-        12%{
+        15%{
             opacity: 1;
         }
-        12.1%{
+        18%{
             opacity: 1;
         }
-        14.1%{
+        20%{
             opacity: 0
         }
         100% {
@@ -345,16 +343,16 @@
         0% {
             opacity: 0;
         }
-        15%{
+        27%{
             opacity: 0;
         }
-        17%{
+        29%{
             opacity: 1;
         }
-        17.1%{
+        32%{
             opacity: 1;
         }
-        19.1%{
+        34%{
             opacity: 0
         }
         100% {
@@ -366,16 +364,16 @@
         0% {
             opacity: 0;
         }
-        20%{
+        43%{
             opacity: 0;
         }
-        22%{
+        45%{
             opacity: 1;
         }
-        22.1%{
+        48%{
             opacity: 1;
         }
-        24.1%{
+        50%{
             opacity: 0
         }
         100% {
@@ -387,123 +385,37 @@
         0% {
             opacity: 0;
         }
-        60%{
-            opacity: 0;
-        }
-        62%{
-            opacity: 1;
-        }
-        62.1%{
-            opacity: 1;
-        }
-        64.1%{
-            opacity: 0
-        }
-        100% {
-            opacity: 0;
-        }
-    }
-
-    @-webkit-keyframes filmpjeTekst_1 {
-        0% {
-            opacity: 0;
-        }
-        5%{
-            opacity: 0;
-        }
-        7%{
-            opacity: 1;
-        }
-        7.1%{
-            opacity: 1;
-        }
-        9.1%{
-            display: none;
-            opacity: 0
-        }
-        100% {
-            display: none;
-            opacity: 0;
-        }
-    }
-
-    @-webkit-keyframes filmpjeTekst_2 {
-        0% {
-            opacity: 0;
-        }
-        10%{
-            opacity: 0;
-        }
-        12%{
-            opacity: 1;
-        }
-        12.1%{
-            opacity: 1;
-        }
-        14.1%{
-            opacity: 0
-        }
-        100% {
-            opacity: 0;
-        }
-    }
-
-    @-webkit-keyframes filmpjeTekst_3 {
-        0% {
-            opacity: 0;
-        }
-        15%{
-            opacity: 0;
-        }
-        17%{
-            opacity: 1;
-        }
-        17.1%{
-            opacity: 1;
-        }
-        19.1%{
-            opacity: 0
-        }
-        100% {
-            opacity: 0;
-        }
-    }
-
-    @-webkit-keyframes filmpjeTekst_4 {
-        0% {
-            opacity: 0;
-        }
-        20%{
-            opacity: 0;
-        }
-        22%{
-            opacity: 1;
-        }
-        22.1%{
-            opacity: 1;
-        }
-        24.1%{
-            opacity: 0
-        }
-        100% {
-            opacity: 0;
-        }
-    }
-
-    @-webkit-keyframes filmpjeTekst_5 {
-        0% {
-            opacity: 0;
-        }
         50%{
             opacity: 0;
         }
         52%{
             opacity: 1;
         }
-        52.1%{
+        60%{
             opacity: 1;
         }
-        54.1%{
+        62%{
+            opacity: 0
+        }
+        300% {
+            opacity: 0;
+        }
+    }
+
+    @keyframes filmpjeTekst_6 {
+        0% {
+            opacity: 0;
+        }
+        70%{
+            opacity: 0;
+        }
+        72%{
+            opacity: 1;
+        }
+        78%{
+            opacity: 1;
+        }
+        80%{
             opacity: 0
         }
         100% {
@@ -511,6 +423,26 @@
         }
     }
 
+    @keyframes filmpjeTekst_7 {
+        0% {
+            opacity: 0;
+        }
+        85%{
+            opacity: 0;
+        }
+        87%{
+            opacity: 1;
+        }
+        95%{
+            opacity: 1;
+        }
+        97%{
+            opacity: 1
+        }
+        100% {
+            opacity: 1;
+        }
+    }
 
 </style>
 
